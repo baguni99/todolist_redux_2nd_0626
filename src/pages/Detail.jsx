6da3378d7484f55649f69c6ac4cc05bda40806b5
eunhee <eunhee9902@gmail.com>
2023-06-26 16:41:05 +0900

    2nd change

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -6,6 +6,8 @@ const Detail = () => {
   const { id } = useParams();
   const navigate = useNavigate();
   const todos = useSelector((state) => state.todos);
+  const comments = useSelector((state)=>state.comments);
+
 
   const todo = todos.filter((todo) => todo.id === id)[0];
 
@@ -20,6 +22,22 @@ const Detail = () => {
       {todo.isDone.toString()}
       <br />
       <button onClick={() => navigate("/")}>이전 화면으로</button>
+
+      <div>
+        <h3>댓글</h3>
+        <form>
+          <input name="이름" value={name} onChange={{(e)=>{
+            setName=(e.target.value)
+          }}}/>
+          <input name="내용"/>
+          <button type=""
+        </form>
+      </div>
+      <div>
+        [comments.map((comment)=>{
+          return <div key={(comment?.id)}></div>
+        })]
+      </div>
     </div>
   );
 };
--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,13 +1,13 @@
 import { createStore } from "redux";
 import { combineReducers } from "redux";
 import todos from "../modules/todos";
-import commentReducer from '../modules/comment'
+import comments from '../modules/comment'
 
 // store를 만들어 내기
 // 중앙데이터관리소
 const rootReducer = combineReducers({
   todos: todos,
-  comments:commentReducer,
+  comments:comments,
 });
 
 const store = createStore(rootReducer);
